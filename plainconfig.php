b8f09f5c9f47a2638f11e5ab8d6d078652886eab
Steven Barker <steve@sphinxsearch.com>
2014-08-22 09:28:14 -0700

    Create plainconfig.php

---
+++ plainconfig.php
@@ -0,0 +1,62 @@
+<?php
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
+session_start();
+require_once('functions.php');
+print_header();
+
+if(!isset($_SESSION["source"])){
+$_SESSION["source"] = array();
+}
+
+
+//if they just jumped here from the navbar link, they may have not set the type.. so, do that.
+if(!isset($_SESSION["type"])){
+$_SESSION["type"] = "mysql";
+}
+
+//if they did set the type, by clicking 'mysql', take that and stuff it into this session variable
+if(isset($_POST["type"])){
+$_SESSION["type"] = $_POST["type"];
+}
+
+//style what comes next
+echo <<<HERE
+<div class='container' style='margin-top:100px'>
+<div class='col-md-12 text-center'>
+	<h3 class="title">Source Options</h3>
+	<div class="row" style="margin-top:50px!important">
+HERE;
+
+//now check what type of source we're building
+switch ($_SESSION["type"]) 
+{
+	case "mysql":
+		
+print_source_form();
+
+		break;
+	case "pssql":
+		echo "Tell Sphinx how to connect to PostgreSQL";
+		break;
+	case "mssql":
+		echo "Tell Sphinx how to connect to Evil Microsoft";
+		break;
+	case "xmlpipe2":
+		echo "Tell Sphinx how to get your XML data";
+		break;
+	case "tsvpipe":
+		echo "Tell Sphinx how to get your TSV data";
+		break;
+	case "odbc":
+		echo "Tell Sphinx how to set up this ODBC connection";
+		break;
+}
+echo <<<HERE
+		</div>
+	</div>
+</div>
+</div>
+HERE;
+?>
