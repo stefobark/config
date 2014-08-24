749dba4e944594cc561613b03ae874ff01e76693
Steven Barker <steve@sphinxsearch.com>
2014-08-24 08:20:25 -0700

    Update final.php

--- final.php
+++ final.php
@@ -4,10 +4,13 @@ session_start();
 print_final_header();
 
 echo "
-		<div class='container' style='margin-top:75px'>
+		<div class='container' style='margin-top:100px'>
 			<div class='row'>
-			<p class='alert alert-info text-center'>now, copy and paste this into some text editor. 
-			save it. name it something like 'sphinxish.conf'</p></div>";
+			<p class='text-center'>now, copy and paste this into some text editor. 
+			save it. name it something like 'sphinxish.conf'<br />
+			
+			<img src='1guysphinx.png' id='copy-button' class='text-center' style='margin-top:25px; margin-bottom:25px!important'><br />
+			<button id='copy-button'>copy to clipboard</button><br /></p></div>";
 
 echo <<<HERE
 <script type="text/javascript">
@@ -29,8 +32,7 @@ print_searchd($_SESSION['searchd']);
 echo "</div>";
 close_output();
 
-echo "<img src='1guysphinx.png' id='copy-button' class='text-center'><br />
-			<button id='copy-button'>copy to clipboard</button><br />
+echo "
 		</div>
 	</div>";
 
