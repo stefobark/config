552a5bd4b9f49d5e41372474f4a5ca1d44ae5c94
Steven Barker <steve@sphinxsearch.com>
2014-09-17 09:23:33 -0700

    Update final.php

--- final.php
+++ final.php
@@ -7,14 +7,7 @@ $scripted = $_SESSION['scripted'];
 echo "
 		<div class='container' style='margin-top:100px'>
 			<div class='row'>
-			<p class='text-center'>Copy and paste this into some text editor. Save it. <br />
-			Name it something like 'sphinxish.conf'. Then, run it. <br /><br />
-			Like this:<br />
-			<p class='help-block text-center'>
-			indexer -c /path/to/your_config.conf index_name <br />
-			</p>
 			<p class='text-center'>
-			List the indexes you want to index or, use '--all' instead of a single index name.<br />
 			<img src='bluesphinx.png' id='copy-button' class='text-center' style='margin-top:25px; margin-bottom:25px!important'><br />
 			<button id='copy-button'>copy to clipboard</button><br /></p></div>";
 
@@ -30,11 +23,19 @@ $("button#copy-button").zclip({
 HERE;
 
 open_output();
-echo "<div id='copy'>
-		$scripted <br /><br />\n";
+echo "<div id='copy'>";
+
+if(strpos($scripted,'bash') !== false){
+		echo "$scripted <br /><br />\n";
+		}
 print_source($_SESSION['source'], $_SESSION['type']);
 print_index($_SESSION['index']);
 print_searchd($_SESSION['searchd']);
+
+if(strpos($scripted,'bash') !== false){
+echo '"';
+}
+
 //close copy div
 echo "</div>";
 close_output();
